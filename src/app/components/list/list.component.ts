afc608a09c553479332a46957786f07b1c7aef00
Abdul Hameed <raza2022@gmail.com>
2018-06-30 18:44:43 +0500

    issue displayed in Mat Table

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -10,13 +10,6 @@ export interface UserData {
   status: string;
 }
 
-/** Constants used to fill up our data base. */
-const COLORS: string[] = ['maroon', 'red', 'orange', 'yellow', 'olive', 'green', 'purple',
-  'fuchsia', 'lime', 'teal', 'aqua', 'blue', 'navy', 'black', 'gray'];
-const NAMES: string[] = ['Maia', 'Asher', 'Olivia', 'Atticus', 'Amelia', 'Jack',
-  'Charlotte', 'Theodore', 'Isla', 'Oliver', 'Isabella', 'Jasper',
-  'Cora', 'Levi', 'Violet', 'Arthur', 'Mia', 'Thomas', 'Elizabeth'];
-
 /**
  * @title Data table with sorting, pagination, and filtering.
  */
@@ -33,25 +26,27 @@ export class ListComponent implements OnInit {
   @ViewChild(MatSort) sort: MatSort;
 
   constructor(public issueService: IssueService) {
-    // Create 100 users
-    const users = Array.from({length: 100}, (_, k) => createNewUser(k + 1));
 
-    // Assign the data to the data source for the table to render
-    this.dataSource = new MatTableDataSource(users);
   }
 
   ngOnInit() {
-    // this.issueService.getIssues().subscribe((data: MatTableDataSource) => {
-    //   console.log(data)
-    //     data.map(obj => {
-    //     return {
-    //       id: obj._id
-    //     };
-    //   })
-    // });
 
-    this.dataSource.paginator = this.paginator;
-    this.dataSource.sort = this.sort;
+    this.issueService.getIssues().subscribe((data) => {
+      if (data instanceof Array) {
+        const users : UserData = data.map(obj => {
+          return {
+            _id: obj._id,
+            title: obj.title,
+            description: obj.description,
+            status: obj.status
+          }
+        });
+        this.dataSource = new MatTableDataSource(users);
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
+      }
+
+    }, error => console.log(error));
   }
 
   applyFilter(filterValue: string) {
@@ -62,22 +57,3 @@ export class ListComponent implements OnInit {
     }
   }
 }
-
-/** Builds and returns a new User. */
-function createNewUser(id: number): UserData {
-  const name =
-    NAMES[Math.round(Math.random() * (NAMES.length - 1))] + ' ' +
-    NAMES[Math.round(Math.random() * (NAMES.length - 1))].charAt(0) + '.';
-
-  return {
-    _id: id.toString(),
-    title: name,
-    description: Math.round(Math.random() * 100).toString(),
-    status: COLORS[Math.round(Math.random() * (COLORS.length - 1))]
-  };
-}
-
-
-/**  Copyright 2018 Google Inc. All Rights Reserved.
- Use of this source code is governed by an MIT-style license that
- can be found in the LICENSE file at http://angular.io/license */
