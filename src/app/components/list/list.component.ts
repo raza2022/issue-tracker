525104dc12e24eda225bbb097aeed8f4d3a4cd96
Abdul Hameed <raza2022@gmail.com>
2018-06-30 16:49:10 +0500

    mat table implemented :rocket:

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -1,17 +1,83 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {MatPaginator, MatSort, MatTableDataSource} from '@angular/material';
 import { IssueService } from '../../issue.service';
+import { map } from "rxjs/internal/operators";
 
+export interface UserData {
+  _id: string;
+  title: string;
+  description: string;
+  status: string;
+}
+
+/** Constants used to fill up our data base. */
+const COLORS: string[] = ['maroon', 'red', 'orange', 'yellow', 'olive', 'green', 'purple',
+  'fuchsia', 'lime', 'teal', 'aqua', 'blue', 'navy', 'black', 'gray'];
+const NAMES: string[] = ['Maia', 'Asher', 'Olivia', 'Atticus', 'Amelia', 'Jack',
+  'Charlotte', 'Theodore', 'Isla', 'Oliver', 'Isabella', 'Jasper',
+  'Cora', 'Levi', 'Violet', 'Arthur', 'Mia', 'Thomas', 'Elizabeth'];
+
+/**
+ * @title Data table with sorting, pagination, and filtering.
+ */
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css']
 })
 export class ListComponent implements OnInit {
-  issues: [Object];
+  displayedColumns: string[] = ['id', 'title', 'description', 'status'];
+  dataSource: MatTableDataSource<UserData>;
 
-  constructor() { }
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
 
-  ngOnInit() {
+  constructor(public issueService: IssueService) {
+    // Create 100 users
+    const users = Array.from({length: 100}, (_, k) => createNewUser(k + 1));
+
+    // Assign the data to the data source for the table to render
+    this.dataSource = new MatTableDataSource(users);
   }
 
+  ngOnInit() {
+    // this.issueService.getIssues().subscribe((data: MatTableDataSource) => {
+    //   console.log(data)
+    //     data.map(obj => {
+    //     return {
+    //       id: obj._id
+    //     };
+    //   })
+    // });
+
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
 }
+
+/** Builds and returns a new User. */
+function createNewUser(id: number): UserData {
+  const name =
+    NAMES[Math.round(Math.random() * (NAMES.length - 1))] + ' ' +
+    NAMES[Math.round(Math.random() * (NAMES.length - 1))].charAt(0) + '.';
+
+  return {
+    _id: id.toString(),
+    title: name,
+    description: Math.round(Math.random() * 100).toString(),
+    status: COLORS[Math.round(Math.random() * (COLORS.length - 1))]
+  };
+}
+
+
+/**  Copyright 2018 Google Inc. All Rights Reserved.
+ Use of this source code is governed by an MIT-style license that
+ can be found in the LICENSE file at http://angular.io/license */
