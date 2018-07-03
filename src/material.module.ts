306cbef3a29ff2214a87a9cbc5be3e82123149e8
Abdul Hameed <raza2022@gmail.com>
2018-07-03 16:04:00 +0500

    basic application views updated :rocket:

--- src/material.module.ts
+++ src/material.module.ts
@@ -12,7 +12,8 @@ import {
   MatToolbarModule,
   MatPaginatorModule,
   MatTableModule,
-  MatSelectModule
+  MatSelectModule,
+  MatListModule
 } from '@angular/material';
 
 @NgModule({
@@ -28,7 +29,8 @@ import {
     MatToolbarModule,
     MatPaginatorModule,
     MatTableModule,
-    MatSelectModule
+    MatSelectModule,
+    MatListModule
   ],
   exports: [
     MatButtonModule,
@@ -42,7 +44,8 @@ import {
     MatToolbarModule,
     MatPaginatorModule,
     MatTableModule,
-    MatSelectModule
+    MatSelectModule,
+    MatListModule
   ],
 })
 export class MaterialModule { }
